c7366b7c965f6c836284ef7aba441b770bbeec25
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-13 19:30:45 +0530

    Create Datatype.java

---
+++ Day2/Datatype.java
@@ -0,0 +1,21 @@
+package Day2;
+
+import java.util.*;
+
+public class Datatype {
+    public static void main(String[] args) {
+        int intNum = 5;
+        double floatNum = 1.6;
+        short shortNum = 246;
+        char charNum = 'a';
+        boolean booleanTrue = true;
+
+        System.out.println("Int: " + intNum);
+        System.out.println("Float: " + floatNum);
+        System.out.println("Short: " + shortNum);
+        System.out.println("char: " + charNum);
+        System.out.println("Boolean: " + booleanTrue);
+
+
+    }
+}
