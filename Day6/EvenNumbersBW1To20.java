ef1d1e5ad86eddf902207e0d50069ec8a56ecff2
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-15 17:26:41 +0530

    Create EvenNumbersBW1To20.java

---
+++ Day6/EvenNumbersBW1To20.java
@@ -0,0 +1,14 @@
+package Day6;
+
+public class EvenNumbersBW1To20 {
+
+    public static void main(String[] args) {
+        int i;
+        for (i=1; i<=20; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+}
