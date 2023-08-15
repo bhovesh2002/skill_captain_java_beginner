649a7aeb9a5b886a726c9b3e76458b4b182db724
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-15 17:27:14 +0530

    Create Numbers1To10.java

---
+++ Day6/Numbers1To10.java
@@ -0,0 +1,12 @@
+package Day6;
+
+public class Numbers1To10 {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 1 ; i<=10 ; i++){
+            System.out.println(i);
+        }
+    }
+
+}
