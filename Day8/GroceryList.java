c5d3e04d4715e381445644af88c9204ef4177c16
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-21 13:13:52 +0530

    Update GroceryList.java

--- Day8/GroceryList.java
+++ Day8/GroceryList.java
@@ -21,34 +21,42 @@ public class GroceryList {
         System.out.println("Press 4 to check a specific item in your list");
         System.out.println("Press 5 to  clear your entire list");
 
-        System.out.println("Please enter your choice: ");
-        int i = Sc.nextInt();
 
-        switch (i) {
-            case 1 -> {
-                System.out.println("Please Enter item name:");
-                String item = Sc.nextLine();
-                groceryList.add(item);
+        int i = 0;
+
+
+        while (i != 6){
+            System.out.println("Please enter your choice: ");
+            i = Sc.nextInt();
+
+            Sc.nextLine();
+
+            switch (i) {
+                case 1 -> {
+                    System.out.println("Please Enter item name:");
+                    String item = Sc.nextLine();
+                    groceryList.add(item);
+                }
+                case 2 -> {
+                    System.out.println("Enter the item you want to remove: ");
+                    String remove = Sc.nextLine();
+                    groceryList.remove(remove);
+                }
+                case 3 -> {
+                    System.out.println("Your Grocery List is as follows: ");
+                    System.out.println(groceryList);
+                }
+                case 4 -> {
+                    System.out.println("Enter the item you want to check: ");
+                    String check = Sc.nextLine();
+                    System.out.println(groceryList.contains(check));
+                }
+                case 5 -> {
+                    groceryList.clear();
+                    System.out.println("Your List has been cleared!");
+                }
+                default -> System.out.println("Wrong number entered");
             }
-            case 2 -> {
-                System.out.println("Enter the item you want to remove: ");
-                String remove = Sc.nextLine();
-                groceryList.remove(remove);
-            }
-            case 3 -> {
-                System.out.println("Your Grocery List is as follows: ");
-                System.out.println(groceryList);
-            }
-            case 4 -> {
-                System.out.println("Enter the item you want to check: ");
-                String check = Sc.nextLine();
-                System.out.println(groceryList.contains(check));
-            }
-            case 5 -> {
-                groceryList.clear();
-                System.out.println("Your List has been cleared!");
-            }
-            default -> System.out.println("Wrong number entered");
         }
 
     }
