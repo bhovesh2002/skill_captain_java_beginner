d7d9348ca5a9bcb4c03df06a7e663443e052c6c4
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-21 12:37:26 +0530

    Create GroceryList.java

---
+++ Day8/GroceryList.java
@@ -0,0 +1,56 @@
+package Day8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GroceryList {
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.println("Welcome to your Grocery List!");
+        List<String> groceryList = new ArrayList<>();
+        groceryList.add("Wheat");
+        groceryList.add("Rice");
+        groceryList.add("Oats");
+
+        System.out.println("Press 1 to add items to your list");
+        System.out.println("Press 2 to remove items to your list");
+        System.out.println("Press 3 to print items in your list");
+        System.out.println("Press 4 to check a specific item in your list");
+        System.out.println("Press 5 to  clear your entire list");
+
+        System.out.println("Please enter your choice: ");
+        int i = Sc.nextInt();
+
+        switch (i) {
+            case 1 -> {
+                System.out.println("Please Enter item name:");
+                String item = Sc.nextLine();
+                groceryList.add(item);
+            }
+            case 2 -> {
+                System.out.println("Enter the item you want to remove: ");
+                String remove = Sc.nextLine();
+                groceryList.remove(remove);
+            }
+            case 3 -> {
+                System.out.println("Your Grocery List is as follows: ");
+                System.out.println(groceryList);
+            }
+            case 4 -> {
+                System.out.println("Enter the item you want to check: ");
+                String check = Sc.nextLine();
+                System.out.println(groceryList.contains(check));
+            }
+            case 5 -> {
+                groceryList.clear();
+                System.out.println("Your List has been cleared!");
+            }
+            default -> System.out.println("Wrong number entered");
+        }
+
+    }
+
+}
