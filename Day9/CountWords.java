bbfc078291bbed9a55187432397d72f403f2e9d8
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-23 13:41:48 +0530

    Create CountWords.java

---
+++ Day9/CountWords.java
@@ -0,0 +1,43 @@
+package Day9;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountWords {
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.println("Enter paragraph or line: ");
+        String para = Sc.nextLine();
+
+        Map<String, Integer> hash = new HashMap<String, Integer>();
+
+        int max = para.length();
+        System.out.println(para.length());
+
+        //removes punctuations and lower cases the whole paragraph
+        String paraPunct = para.replaceAll("\\p{Punct}","").toLowerCase();
+
+        //splits the paragraph into words
+        String[] words = paraPunct.split(" ");
+
+        //if the key doesnt exists, it will create one with value 1, otherwise it will replace the previous key when it 
+        // finds that the key exists and also increase the value by 1
+        for(String token: words) {
+            if(!hash.containsKey(token)){
+                hash.put(token, 1);
+            }else {
+                hash.put(token, hash.get(token) + 1);
+            }
+        }
+
+        //print words with their frequency
+        System.out.println(hash);
+
+        System.out.println(para);
+
+    }
+
+}
